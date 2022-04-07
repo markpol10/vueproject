fbe96fa96215f84fb9c2893447130a0d8857b5c0
markpol10 <34650256+markpol10@users.noreply.github.com>
2022-04-07 12:50:02 +0200

    eerste commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="page-container">
+
+  <div> 
+  <peoplespace />
+  <firstcomponent />
+  <spacestation />
   </div>
+
+<div class="mars"></div>
+
+  <img src="./components/world.png" class="world"> 
+
+
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import firstcomponent from './components/FirstComponent'
+import peoplespace from './components/peoplespace'
+import spacestation from './components/spacestation'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    peoplespace,
+    firstcomponent,
+    spacestation
   }
-}
+};
+
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+
+body {
+  margin: 0;
+  padding: 0;
+  color: white;
+
 }
+
+.page-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(43,57,129,1) 19%, rgba(27,27,27,1) 64%);
+}
+
+.world {
+  position: absolute;
+  height: 400px;
+  z-index: 0;
+}
+
+.world:hover {
+  transform: rotate(20deg);
+  transition: 0.5s;
+}
+
+.mars {
+  position: absolute;
+  bottom: 0;
+  height: 50px;
+  width: 100vw;
+  background: rgba(161, 110, 74, 1);
+  z-index: 0;
+}
+
+
+
 </style>
